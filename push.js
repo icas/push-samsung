394d888a79ee16fb650b70f37cc3c2441f535985
Ivan Ardila <icasardila@gmail.com>
2014-01-15 10:25:12 -0500

    recieve form data and push to all

--- push.js
+++ push.js
@@ -3,126 +3,68 @@ var UA = require('urban-airship');
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var qs = require('querystring');
 
 
 
 
 app.use(logfmt.requestLogger());
-app.get('/', function(req, res) {
-  var ua = new UA("kVo8mAGBSa6qS7rDLy7PHA", "6h1idFZmS6a88jpBOYYk3w", "dJ_f1q35TCqurLiDfjbLPw");
 
-	var payload0 = {
-	  "audience" : {
-	      "apid" : "30c570f0-eb26-4558-b0e1-ef7046d11528"
-	  },
-	  "notification" : {
-	       "alert" : "Hello!",
-	       "android": {
-	       	"extra": {"roomCode": "12345"}
-	       }
-	       
-	  },
-	  "device_types" :  "all"
-	};
+app.post('/push', function(request, response) {
+  console.log("aaa")
+  var body = "";
+  request.on('data', function (data) {
+      body += data;
+      // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+      if (body.length > 1e6) { 
+          // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+          request.connection.destroy();
+      }
+  });
 
-	ua.pushNotification("/api/push", payload0, function(error) {
-		console.log(error)
-	});
+  request.on('end', function () {
+      var keys = []
+      var postData = qs.parse(body);
+      for(var key in postData){
+        keys.push(key);
+      }
+      
+      Pusher.pushToAll(JSON.parse(keys[0]));
+      // use POST
+  });
 
-	res.send('Hello World!');
+    //res.send('Hello World!');
 
 });
+
+
+Pusher = {};
+Pusher.pushToAll = function(data){
+  console.log(data.android.alert);
+  console.log(data.roomCode);
+  
+  
+  var ua = new UA("kVo8mAGBSa6qS7rDLy7PHA", "6h1idFZmS6a88jpBOYYk3w", "dJ_f1q35TCqurLiDfjbLPw");
+  var payload0 = {
+    "audience" : "all",
+    "notification" : {
+         "alert" : data.android.alert,
+         "android": {
+          "extra": {"roomCode": data.roomCode}
+         }
+         
+    },
+    "device_types" :  "all"
+  };
+
+  ua.pushNotification("/api/push", payload0, function(error) {
+    console.log(error)
+  });
+
+  
+};
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-
-
-//Fixing this method, adding headers
-// UA.prototype._transport = function(path, method, request_data, callback) {
-// 	var self = this,
-// 		rd = "",
-// 		response_data = "",
-// 		https_opts = {
-// 			"host": "go.urbanairship.com",
-// 			"port": 443,
-// 			"path": path,
-// 			"method": method,
-// 			"headers": {
-// 				"Authorization": "Basic " + this._auth,
-// 				"User-Agent": "node-urban-airship/0.2",
-// 				"Accept": "application/vnd.urbanairship+json; version=3;"
-// 			}
-// 		};
-	
-// 	// We don't necessarily send data
-// 	if (request_data instanceof Function) {
-// 		callback = request_data;
-// 		request_data = null;
-// 	}
-	
-// 	// Set a Content-Type and Content-Length header if we are sending data
-// 	if (request_data) {
-// 		rd = JSON.stringify(request_data);
-		
-// 		https_opts.headers["Content-Type"] = "application/json";
-// 		https_opts.headers["Content-Length"] = Buffer.byteLength(rd, "utf8");
-// 	}
-// 	else {
-// 		https_opts.headers["Content-Length"] = 0;
-// 	}
-	
-// 	var request = https.request(https_opts, function(response) {
-// 		response.setEncoding("utf8");
-		
-// 		response.on("data", function(chunk) {
-// 			response_data += chunk;
-// 		});
-		
-// 		response.on("end", function() {
-// 			// You probably forget the trailing '/'
-// 			if ((response.statusCode == 301 || response.statusCode == 302) && response.headers && response.headers.location) {
-// 				var url = require("url"),
-// 					parsed_url = url.parse(response.headers.location);
-					
-// 				self._transport(parsed_url.pathname + (parsed_url.search || ""), method, request_data, callback);
-// 			}
-// 			// Success on 200 or 204, 201 on new device registration
-// 			else if ([200,201,204].indexOf(response.statusCode) >= 0) {
-// 				try {
-// 					switch (true) {
-// 						case /application\/json/.test(response.headers["content-type"]):
-// 							callback(null, JSON.parse(response_data));
-// 							break;
-// 						default:
-// 							callback(null, response_data);
-// 					}
-// 				}
-// 				catch (ex) {
-// 					callback(ex);
-// 				}
-// 			}
-// 			else {
-// 				callback(new Error(response_data));
-// 			}
-// 		});
-		
-// 		response.on("error", function(error) {
-// 			callback(error);
-// 		});
-// 	});
-	
-// 	request.on("error", function(error) {
-// 		callback(error);
-// 	});
-	
-// 	if (request_data) {
-// 		request.write(rd);
-// 	}
-	
-// 	request.end();
-// };
-
-
